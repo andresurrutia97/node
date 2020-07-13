78581ca467b865d5d41558a1c87240ba46a4bdfe
User <user@MacBook-Pro-de-AndresU.local>
2020-07-13 17:40:36 -0500

    config

--- fs-scan.js
+++ fs-scan.js
@@ -4,15 +4,20 @@ const myArgs = process.argv.slice(2);
 
 const getFiles = (currentPath = __dirname) => {
   const files = fs.readdirSync(currentPath);
+
   const filter = new RegExp(myArgs);
-  files.map((el) => {
-    const currentFile = `${currentPath}/${el}`;
+
+  files.map((file) => {
+    const currentFile = `${currentPath}/${file}`;
     const stats = fs.statSync(currentFile);
 
-    if (stats.isFile() && filter.test(el)) {
+    if (stats.isFile() && filter.test(file)) {
       console.log(currentFile);
     } else if (stats.isDirectory()) {
       getFiles(currentFile);
     }
+    return currentFile;
   });
 };
+
+getFiles();
